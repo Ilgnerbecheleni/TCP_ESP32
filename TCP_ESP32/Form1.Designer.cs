56c97fb5f9a264b5624fde0e8c30a7e4df6da86e
Ilgner Aparecido Becheleni <81272773+Ilgnerbecheleni@users.noreply.github.com>
2022-04-17 23:58:55 -0300

    Adicionar arquivos de projeto.

---
+++ TCP_ESP32/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TCP_ESP32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.txtPorta = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btn_desconectar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_enviar = new System.Windows.Forms.Button();
+            this.txtEnviar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.Location = new System.Drawing.Point(196, 60);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_conectar.TabIndex = 0;
+            this.btn_conectar.Text = "Conectar";
+            this.btn_conectar.UseVisualStyleBackColor = true;
+            this.btn_conectar.Click += new System.EventHandler(this.btn_conectar_Click);
+            // 
+            // txtPorta
+            // 
+            this.txtPorta.Location = new System.Drawing.Point(80, 63);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(100, 20);
+            this.txtPorta.TabIndex = 1;
+            this.txtPorta.Text = "23";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(80, 39);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 20);
+            this.txtHost.TabIndex = 2;
+            this.txtHost.Text = "192.168.0.111";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(41, 132);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(230, 105);
+            this.textBox3.TabIndex = 3;
+            // 
+            // btn_desconectar
+            // 
+            this.btn_desconectar.Location = new System.Drawing.Point(277, 60);
+            this.btn_desconectar.Name = "btn_desconectar";
+            this.btn_desconectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_desconectar.TabIndex = 4;
+            this.btn_desconectar.Text = "Desconectar";
+            this.btn_desconectar.UseVisualStyleBackColor = true;
+            this.btn_desconectar.Click += new System.EventHandler(this.btn_desconectar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Porta";
+            // 
+            // btn_enviar
+            // 
+            this.btn_enviar.Location = new System.Drawing.Point(277, 100);
+            this.btn_enviar.Name = "btn_enviar";
+            this.btn_enviar.Size = new System.Drawing.Size(75, 59);
+            this.btn_enviar.TabIndex = 7;
+            this.btn_enviar.Text = "Enviar";
+            this.btn_enviar.UseVisualStyleBackColor = true;
+            this.btn_enviar.Click += new System.EventHandler(this.btn_enviar_Click);
+            // 
+            // txtEnviar
+            // 
+            this.txtEnviar.Location = new System.Drawing.Point(41, 100);
+            this.txtEnviar.Name = "txtEnviar";
+            this.txtEnviar.Size = new System.Drawing.Size(230, 20);
+            this.txtEnviar.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 249);
+            this.Controls.Add(this.txtEnviar);
+            this.Controls.Add(this.btn_enviar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_desconectar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.txtPorta);
+            this.Controls.Add(this.btn_conectar);
+            this.Name = "Form1";
+            this.Text = "TCP CHAT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_conectar;
+        private System.Windows.Forms.TextBox txtPorta;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btn_desconectar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_enviar;
+        private System.Windows.Forms.TextBox txtEnviar;
+    }
+}
+
---
+++ TCP_ESP32/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Net.Sockets;
+using SimpleTCP;
+using System.Windows.Forms;
+
+namespace TCP_ESP32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpClient client;
+
+        private void btn_conectar_Click(object sender, EventArgs e)
+        {
+            client.Connect(txtHost.Text, Convert.ToInt32(txtPorta.Text));
+            btn_desconectar.Enabled = true;
+
+
+            if (client.TcpClient.Connected == true)
+            {
+
+                btn_conectar.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+
+            client.StringEncoder = Encoding.UTF8;
+
+            client.DataReceived += Client_DataReceived;
+
+
+
+
+        }
+
+        private void Client_DataReceived (object sender , SimpleTCP.Message e)
+        {
+            textBox3.Invoke((MethodInvoker)delegate () {
+
+                textBox3.Text += e.MessageString;         
+            
+            
+            
+            });
+        }
+
+        private void btn_desconectar_Click(object sender, EventArgs e)
+        {
+            if (client.TcpClient.Connected == true)
+            {
+                client.Disconnect();
+                btn_conectar.Enabled = true;
+                btn_desconectar.Enabled = false;
+            }
+        }
+
+        private void btn_enviar_Click(object sender, EventArgs e)
+        {
+            if (client.TcpClient.Connected == true)
+            {
+                client.Write(txtEnviar.Text+"\n"+"\r");
+            }
+
+        }
+    }
+}
